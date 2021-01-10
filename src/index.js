9ea7e0e0c427e0c56faa54a50bfbb00523cf83e7
Stas Miotk <tgb.stas@hotmail.com>
2021-01-10 14:31:02 +0100

    Add & import file App.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
+import App from './App';
 
 const destination = document.querySelector("#container");
 
