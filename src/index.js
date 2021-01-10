1001c2fc3694bdd8e94e49ba5439b5cf7b027988
Stas Miotk <tgb.stas@hotmail.com>
2021-01-10 14:27:49 +0100

    Create file index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const destination = document.querySelector("#container");
+
+ReactDOM.render(
+  <div></div>,
+  destination
+);
