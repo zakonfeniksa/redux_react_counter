5085e5f72df62446d9e7d28f76b7fc2eb034068c
Stas Miotk <tgb.stas@hotmail.com>
2021-01-10 14:32:37 +0100

    Add file reducer.js & import

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import counter from './reducer';
 
 const destination = document.querySelector("#container");
 
